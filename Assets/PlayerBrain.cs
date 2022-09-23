38f3085a6546f6c0049a9bb71abbd4dd715a6cfa
Chichou <ch1ch0ud3v@gmail.com>
2022-09-23 15:48:38 +0200

    camera brain modification
    script PlayerBrain modification

--- Assets/PlayerBrain.cs
+++ Assets/PlayerBrain.cs
@@ -8,24 +8,59 @@ public class PlayerBrain : MonoBehaviour
     [SerializeField] InputActionReference _move;
     //[SerializeField] InputActionReference _interraction;
     //[SerializeField] InputActionReference _jump;
-    //[SerializeField] InputActionReference _sprint;
+    [SerializeField] InputActionReference _sprint;
     [SerializeField] Rigidbody _rb;
     [SerializeField] float _speed;
+    [SerializeField] float _accelerate;
 
-    Vector3 Direction ;
+    Vector3 _direction;
+    bool _isRunning;
+    Vector3 Direction
+    {
+        get => _direction;
+        set => _direction = value.normalized;
+    }
 
     private void Start ()
     {
-        _move.action.started += StartMove;        
+        _move.action.started += StartMove;
+        _move.action.performed += StartMove;
+        _move.action.canceled += EndMove;
+        _sprint.action.started += SprintStart;
+        _sprint.action.canceled += SprintEnd;
+    }
+
+
+    private void SprintStart (InputAction.CallbackContext obj)
+    {
+        _isRunning = true;
+    }
+
+    private void SprintEnd (InputAction.CallbackContext obj)
+    {
+        _isRunning = false;
     }
 
     private void FixedUpdate ()
     {
-        _rb.MovePosition (_direction * _speed * Time.fixedDeltaTime );
+        if (_isRunning)
+        {
+            _rb.MovePosition (_rb.transform.position +_direction * Time.fixedDeltaTime * (_speed * _accelerate));
+        }
+        else
+        {
+            _rb.MovePosition (_rb.transform.position + _direction * _speed * Time.fixedDeltaTime );
+        }
     }
 
     private void StartMove (InputAction.CallbackContext obj)
     {
         _direction = obj.ReadValue<Vector2> ();
+        _direction = new Vector3(_direction.x, 0, _direction.y);
+    }
+
+    private void EndMove (InputAction.CallbackContext obj)
+    {
+        _direction = Vector3.zero;
     }
 }
