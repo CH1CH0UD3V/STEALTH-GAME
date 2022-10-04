6e0c50e3757394885bde722d6319639e72b3ac29
Chichou <ch1ch0ud3v@gmail.com>
2022-10-05 01:20:44 +0200

    update

--- Assets/HitBoxEnemy.cs
+++ Assets/HitBoxEnemy.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class HitBoxEnemy : MonoBehaviour
 {
     [SerializeField] BlockAttack _attack;
+    [SerializeField] float _distanceMin;
+    //[SerializeField] float _heightRay;
+    [SerializeField] float _rayDistance;
+    [SerializeField] Transform Enemy;
+
+    private void Reset ()
+    {
+        _distanceMin = 2f;
+    }
     private void OnTriggerEnter (Collider other)
     {
-        if(Vector3.Distance(_playerCc,_distanceMin))
+        float _distance = Vector3.Distance (other.transform.position, transform.position);
+        Debug.DrawLine (Enemy.transform.position + Vector3.up /** _heightRay*/, Vector3.forward * _rayDistance,Color.yellow);
+        if (_distance < _distanceMin)
         {
-
+            _attack.LaunchAttack ();
         }
     }
 }
