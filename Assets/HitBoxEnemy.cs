ae71e0ae932d49ae3692e95c7775da7ebfd71c45
Chichou <ch1ch0ud3v@gmail.com>
2022-10-05 01:20:20 +0200

    update

--- Assets/HitBoxEnemy.cs
+++ Assets/HitBoxEnemy.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class HitBoxEnemy : MonoBehaviour
 {
     [SerializeField] BlockAttack _attack;
-    [SerializeField] float _distanceMin;
-    [SerializeField] GameObject Player _playerCc;
     private void OnTriggerEnter (Collider other)
     {
         if(Vector3.Distance(_playerCc,_distanceMin))
