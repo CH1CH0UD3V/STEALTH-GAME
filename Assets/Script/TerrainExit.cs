ac8d55a4e00ae90fb2322eb7774f4768c3d505dd
Chichou <ch1ch0ud3v@gmail.com>
2022-11-09 00:51:49 +0100

    modification terrain(nivelage terrain en cours)

--- Assets/Script/TerrainExit.cs
+++ Assets/Script/TerrainExit.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TerrainExit : MonoBehaviour
 {
-    [SerializeField] CharacterController _cc;
-    [SerializeField] Vector3 _notAdmitt;
+    //[SerializeField] CharacterController _cc;
+    //[SerializeField] Vector3 _notAdmitt;
+    //[SerializeField] SpawnPoint _spwaney ;
 
     private void OnTriggerExit (Collider other)
     {
         if (other.GetComponentInParent<PlayerTag> ())
         {
-            if(_cc.isGrounded == false)
-            {
-                _cc.transform.position = Vector3.zero;
-                _cc.Move (_notAdmitt);
-            }
+            SceneManager.LoadScene (0);
+            //if(_cc.isGrounded == false)
+            //{
+            //    _cc.transform.position = Vector3.zero;
+            //    _cc.Move (_notAdmitt);
+            //}
         }
     }
 }
