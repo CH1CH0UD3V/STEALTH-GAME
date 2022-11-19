5b705393ba56fa25edbc4a53df7636279d3e83a4
Chichou <ch1ch0ud3v@gmail.com>
2022-11-19 06:31:42 +0100

    Copie du terrain avec le village que j'ai mis au dessus de la map originale pour faire une Didactiel
    j'ai crée des animations pour les maisons et pour les UI, j'ai mis un son que j'ai crée provisoirement.

--- Assets/Disable_EnableUI.cs
+++ Assets/Disable_EnableUI.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Disable_EnableUI : MonoBehaviour
 {
+    [Header("UI")]
     [SerializeField] GameObject _firstUi;
     [SerializeField] GameObject _secondUi;
+    [Header("Animation")]
+    [SerializeField] Animator _animationHousePair1;
+    [SerializeField] Animator _animationHousePair2;
+
+    bool _firstPassed;
+    bool _secondPassed;
 
 
     private void OnTriggerEnter (Collider other)
@@ -13,14 +20,12 @@ public class Disable_EnableUI : MonoBehaviour
         var player = other.GetComponentInParent<PlayerTag> ();
         if (player != null)
         {
-            StartCoroutine (await());
+            _firstUi.SetActive (false);
+            _secondUi.SetActive (true);
+            _firstPassed = true;
+            _secondPassed = true;
+            _animationHousePair1.SetBool ("Passed", _firstPassed);
+            _animationHousePair2.SetBool ("SecondPassed", _secondPassed);
         }
-    }
-
-    IEnumerator await ()
-    {
-        _firstUi.SetActive (false);
-        yield return new WaitForSeconds(5);
-        _secondUi.SetActive (true);
     }
 }
